64faaedc9db8f5910226d88f33233983cc8be1ce
Gleb Mickevich <glebmickevich@gmail.com>
2022-01-18 23:28:29 +0200

    Lesson 1: Greetings

---
+++ gb_c-sharp-basics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace gb_c_sharp_basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! Today is {DateTime.Now}");
+            Console.ReadKey();
+        }
+    }
+}
