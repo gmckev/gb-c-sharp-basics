7a46410900d6bcf07cbb4ede3f3502d59a53bc0a
Gleb Mickevich <glebmickevich@gmail.com>
2022-03-16 05:18:14 +0200

    Lesson 5 Task 4: Directory to file

---
+++ Lesson_5_Task_4/DirectoryToFile.cs
@@ -0,0 +1,19 @@
+﻿//Сохранить дерево каталогов и файлов по заданному пути в текстовый файл — с рекурсией и без.
+
+Console.WriteLine("Enter directory path");
+string userDirectoryPath = Console.ReadLine();
+
+string[] entries = Directory.GetFileSystemEntries(userDirectoryPath, "*", SearchOption.AllDirectories);
+
+string fileName = "pathToFile.txt";
+File.WriteAllLines(fileName, entries);
+
+
+string[] file = File.ReadAllLines(fileName);
+
+foreach (string entry in file)
+{
+    Console.WriteLine(entry);
+}
+
+
