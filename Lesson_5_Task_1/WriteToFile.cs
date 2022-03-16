1aa74d60a60452e03210abadaa46c4329869fabb
Gleb Mickevich <glebmickevich@gmail.com>
2022-03-16 05:02:58 +0200

    Lesson 5 Task 1: Writing to file

---
+++ Lesson_5_Task_1/WriteToFile.cs
@@ -0,0 +1,6 @@
+﻿// Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+
+Console.WriteLine("Enter any text");
+string userInput = Console.ReadLine();
+string fileName = "new.txt";
+File.WriteAllText(fileName, userInput);
